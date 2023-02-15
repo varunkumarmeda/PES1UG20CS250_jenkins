be959b3a73bd80676b90b8e51a761dc7e4561f10
Meda Varun Kumar <77848108+varunkumarmeda@users.noreply.github.com>
2023-02-15 23:25:44 +0530

    Create temp.cpp

---
+++ main/temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World This is MEDA VARUN KUMAR\n";
+  cout << "I am in Task5 my SRN is PES1UG20CS250\n";
+  return 0;
+}
